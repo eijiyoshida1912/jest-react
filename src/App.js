b5cc1f77d41a1f0ae4eb75e34180e96d9f78c103
EijiYoshida <eijiyoshida1912@gmail.com>
2022-07-01 22:41:40 +0900

    17:35まで完了

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login/Login';
 
 function App() {
+  const a = 2;
+  const b = 4;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Login />
+      {/*
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn react
         </a>
-      </header>
+        <ul>
+          <li>Apple</li>
+          <li>Banana</li>
+          <li>Orange</li>
+        </ul>
+        <h1 data-testid="mytestid">Hello</h1>
+        <span title="sum">{a+b}</span>
+  */}
     </div>
   );
 }
